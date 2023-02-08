079f02fde229d94196f155e3470338f5f402e2c1
guangkun0818 <609946862@qq.com>
2023-02-09 00:19:35 +0800

    [frontend_sample_data] - add sample_data for unittest.

--- dataset/frontend/frontend_test.py
+++ dataset/frontend/frontend_test.py
@@ -16,10 +16,11 @@ class FrontendTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self._frontend = EcapaFrontend()
-        # TODO: Set test wavs
+        self._test_data_1 = "sample_data/data/wavs/251-136532-0007.wav"
+        self._test_data_2 = "sample_data/data/wavs/1462-170138-0015.wav"
 
     def test_frontend(self):
-        pcms = torch.rand(1, 32000)
+        pcms, _ = torchaudio.load(self._test_data_1)
         feats = self._frontend(pcms)
         glog.info("MFCC feature: {}".format(feats.shape))
         # According to paper http://arxiv.org/pdf/2010.11255.pdf
@@ -29,14 +30,14 @@ class FrontendTest(unittest.TestCase):
 
     def test_frontend_torchscript(self):
         # Frontend torchscript export unittest
-        pcms = torch.rand(1, 32000)
-        pt_feats = self._frontend(pcms)
-
+        pcms, _ = torchaudio.load(self._test_data_1)
         torchscript_frontend = torch.jit.trace(self._frontend,
                                                example_inputs=pcms)
         torchscript_frontend = torch.jit.script(torchscript_frontend)
 
         # Torchscript frontend precision check
+        pcms, _ = torchaudio.load(self._test_data_2)
+        pt_feats = self._frontend(pcms)
         ts_feats = torchscript_frontend(pcms)
         glog.info("Torchscript output :{}".format(ts_feats.shape))
         glog.info("Checkpoint output: {}".format(pt_feats.shape))
@@ -47,7 +48,9 @@ class KaldiFbankTest(unittest.TestCase):
     """ Unittest of KaldiWaveFeature frontend """
 
     def setUp(self) -> None:
-        # TODO: Set test wavs
+        self._test_data_1 = "sample_data/data/wavs/251-136532-0007.wav"
+        self._test_data_2 = "sample_data/data/wavs/1462-170138-0015.wav"
+
         self._config = {
             "feat_config": {
                 "num_mel_bins": 80,
@@ -61,14 +64,14 @@ class KaldiFbankTest(unittest.TestCase):
 
     def test_frontend_wave_feature(self):
         # Frontend forward unittest
-        pcms = torch.rand(1, 32000)
+        pcms, _ = torchaudio.load(self._test_data_1)
         feats = self._kaldi_frontend(pcms)
         glog.info("Fbank feature: {}".format(feats.shape))
         self.assertEqual(feats.shape[-1], self._kaldi_frontend._num_mel_bins)
 
     def test_frontend_torchscript(self):
         # Frontend torchscript export unittest
-        
+
         pcms = torch.rand(1, 41360)
         # NOTE: Shit happens here, If we use pcms other than shape == (1, 41360) along with
         # speechbrain import, RuntimeError pops up: The size of tensor a (323) must match
