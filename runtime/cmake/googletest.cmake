9dced5d5e0ddd6ce507590fd13ec9e86db19cb64
guangkun0818 <609946862@qq.com>
2023-02-14 01:56:50 +0800

    [embedding_model_impl] - cmake formatted.

--- runtime/cmake/googletest.cmake
+++ runtime/cmake/googletest.cmake
@@ -1,11 +1,10 @@
 # GoogleTest
 FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.13.0
-)
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG v1.13.0)
 FetchContent_MakeAvailable(googletest)
 
-include_directories(${googletest_SOURCE_DIR}/googletest/include
-                    ${googletest_SOURCE_DIR}/googlemock/include
-                    ${googletest_BINARY_BIN})
+include_directories(
+  ${googletest_SOURCE_DIR}/googletest/include
+  ${googletest_SOURCE_DIR}/googlemock/include ${googletest_BINARY_BIN})
