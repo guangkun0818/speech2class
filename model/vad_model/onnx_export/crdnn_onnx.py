1d505774a1b810a728494355781d926fd8d69c0e
guangkun0818 <609946862@qq.com>
2023-03-05 03:15:47 +0800

    [crdnn_onnx] - code formatted.

--- model/vad_model/onnx_export/crdnn_onnx.py
+++ model/vad_model/onnx_export/crdnn_onnx.py
@@ -24,14 +24,16 @@ class CrdnnOnnxInit(Crdnn):
         # Override original forward as initialize_cache
         return self.initialize_cache()
 
-    
+
 class CrdnnOnnxInference(Crdnn):
     """ Crdnn Inference for onnx export """
 
     def __init__(self, config: CrdnnConfig):
         super(CrdnnOnnxInference, self).__init__(config)
 
-    def forward(self, x: torch.Tensor, cache: Tuple[List[torch.Tensor], Tuple[torch.Tensor, torch.Tensor]]):
+    def forward(self, x: torch.Tensor, cache: Tuple[List[torch.Tensor],
+                                                    Tuple[torch.Tensor,
+                                                          torch.Tensor]]):
         # Override original forward as inference
         logits, next_cache = self.inference(x, cache)
 
--- model/vad_model/onnx_export/crdnn_onnx_test.py
+++ model/vad_model/onnx_export/crdnn_onnx_test.py
@@ -16,10 +16,10 @@ class TestCrdnnOnnxExport(unittest.TestCase):
     """ Unittest of both CrdnnOnnxInit and CrdnnOnnxInference, 
         using tiny Crdnn setting, 277k in quant torchscript
     """
-    
+
     def setUp(self) -> None:
-        self._cache_nums = 3 + 2 # 3 layers Cnn, 2 for Rnn cache
-        self._config  = {
+        self._cache_nums = 3 + 2  # 3 layers Cnn, 2 for Rnn cache
+        self._config = {
             "cnn_block_config": {
                 "num_layers": 3,
                 "conv_type": "conv2d",
@@ -42,32 +42,34 @@ class TestCrdnnOnnxExport(unittest.TestCase):
         }
 
         self._crdnn_init = CrdnnOnnxInit(config=CrdnnConfig(**self._config))
-        self._crdnn_infer = CrdnnOnnxInference(config=CrdnnConfig(**self._config))
+        self._crdnn_infer = CrdnnOnnxInference(config=CrdnnConfig(
+            **self._config))
 
         self._dummy_feats = torch.rand(1, 100, 64)
         self._dummy_cache = self._crdnn_init.initialize_cache()
 
     def test_crdnn_init_onnx_export(self):
         # Unittest of CrdnnOnnxInit export
-        # Onnx export require args, use dummy_input 1 of it. However 
-        # dummy_input cant not be traced within onnx model, therefore 
+        # Onnx export require args, use dummy_input 1 of it. However
+        # dummy_input cant not be traced within onnx model, therefore
         # input_feed of OrtSession should be empty.
-        output_names=["cache_%d" % i for i in range(5)]
+        output_names = ["cache_%d" % i for i in range(5)]
         torch.onnx.export(self._crdnn_init,
                           args=1,
-                          f="test_logs/crdnn_init.onnx", 
-                          verbose=True, 
-                          input_names=None, 
+                          f="test_logs/crdnn_init.onnx",
+                          verbose=True,
+                          input_names=None,
                           output_names=output_names)
-        
-        torch_output = self._crdnn_init(1) # dummy_input
-        
+
+        torch_output = self._crdnn_init(1)  # dummy_input
+
         ort_session = ort.InferenceSession("test_logs/crdnn_init.onnx")
         # Output of onnx were flattened as list[*cnn_cache, *rnn_cache]
         onnx_output = ort_session.run(None, input_feed={})
-        
+
         # Cache check
-        for th_out, onnx_out in zip([*torch_output[0], *torch_output[1]], onnx_output):
+        for th_out, onnx_out in zip([*torch_output[0], *torch_output[1]],
+                                    onnx_output):
             torch.allclose(th_out, torch.Tensor(onnx_out))
 
     def test_crdnn_infer_onnx_export(self):
@@ -76,29 +78,36 @@ class TestCrdnnOnnxExport(unittest.TestCase):
         input_names = ["feats"] + ["cache_%d" % i for i in range(5)]
         output_names = ["logits"] + ["next_cache_%d" % i for i in range(5)]
 
-        torch.onnx.export(self._crdnn_infer, 
-                          (self._dummy_feats, self._dummy_cache), 
-                          "test_logs/crdnn_inference.onnx", 
-                          verbose=True, 
-                          input_names=input_names, 
+        torch.onnx.export(self._crdnn_infer,
+                          (self._dummy_feats, self._dummy_cache),
+                          "test_logs/crdnn_inference.onnx",
+                          verbose=True,
+                          input_names=input_names,
                           output_names=output_names)
 
-        torch_logits, torch_cache = self._crdnn_infer(self._dummy_feats, self._dummy_cache)
-        
+        torch_logits, torch_cache = self._crdnn_infer(self._dummy_feats,
+                                                      self._dummy_cache)
+
         ort_session = ort.InferenceSession("test_logs/crdnn_inference.onnx")
-    
+
         # Output of onnx were flattened as list[*cnn_cache, *rnn_cache]
         dummy_cache = {}
-        for i, cache in enumerate([*self._dummy_cache[0], *self._dummy_cache[1]]):
+        for i, cache in enumerate(
+            [*self._dummy_cache[0], *self._dummy_cache[1]]):
             dummy_cache["cache_{}".format(i)] = cache.numpy()
-        
-        # Outputs of Onnx: ["logits", next_cache_0, ...]
-        onnx_output = ort_session.run(
-            ["logits"] + ["next_cache_%d" % i for i in range(5)], 
-            input_feed={"feats": self._dummy_feats.numpy(), **dummy_cache})
 
-        for th_out, onnx_out in zip([torch_logits] + [*torch_cache[0], *torch_cache[1]], onnx_output):
+        # Outputs of Onnx: ["logits", next_cache_0, ...]
+        onnx_output = ort_session.run(["logits"] +
+                                      ["next_cache_%d" % i for i in range(5)],
+                                      input_feed={
+                                          "feats": self._dummy_feats.numpy(),
+                                          **dummy_cache
+                                      })
+
+        for th_out, onnx_out in zip(
+            [torch_logits] + [*torch_cache[0], *torch_cache[1]], onnx_output):
             torch.allclose(th_out, torch.Tensor(onnx_out))
-    
+
+
 if __name__ == "__main__":
     unittest.main()
